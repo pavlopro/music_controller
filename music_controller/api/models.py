a16a6905824ab3a9b629a937bda2896892899ee4
pavlopro <pavlop@enapps.co.uk>
2021-02-22 22:20:30 +0200

    [DEV] added Room model, serializer class and related view;

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import string
+import random
 
-# Create your models here.
+
+def generate_unique_code():
+    length = 6
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if not Room.objects.fiter(code=code):
+            break
+    return code
+
+
+class Room(models.Model):
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.PositiveSmallIntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
